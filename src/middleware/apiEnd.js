c142840a4e75007671c8fee27718e94d8165182d
maeve <maeve.oh@flowerfarm.co.kr>
2022-11-15 19:10:13 +0900

    commit

--- src/middleware/apiEnd.js
+++ src/middleware/apiEnd.js
@@ -1,10 +1,12 @@
 const apiEnd = async (req, res, next) => {
 	try {
 		res.status(res.locals.status === 0 ? 500 : res.locals.status).send(res.locals.data);
+		console.log('1');
 	}
 	catch (error) {
 		res.locals.error = error;
-		res.status(500).send({ message: '[03]시스템 오류가 발생했습니다.'})
+		console.log('2');
+		res.status(500).send({ message: '[02] 시스템 오류가 발생했습니다.'})
 	}
 };
 
--- src/v2/auth/login.js
+++ src/v2/auth/login.js
@@ -1,4 +1,7 @@
 const db = require('../../config/database');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwtKey = require('../../config/secretKey');
 
 exports.login = async (req, res, next) => {
 	const { id, password } = req.body;
@@ -19,15 +22,60 @@ exports.login = async (req, res, next) => {
 			return false;
 		}
 
-		const result = await conn.query(`SELECT * FROM user_tb WHERE isDelete='N' AND id=?`, [id]);
-		// console.log(result[0]);
-		// res.send(result[0][0]);
-		const data = result[0][0];
-		throw { message: `${data}` };
+		// id 조회
+		const sql1 = `
+			SELECT
+				*
+			FROM user_tb
+			WHERE isDelete='N' AND id=${conn.escape(id)}
+		`
+
+		const result1 = await conn.query(sql1);
+		const user = result1[0][0];
+		// id 조회 실패
+		if (!user) {
+			res.locals.status = 400;
+			res.locals.data = { message: '존재하지 않는 id입니다. 다시 한 번 확인해주세요.' };
+			next();
+			return false;
+		}
+
+		// 비밀번호 확인
+		const result2 = bcrypt.compareSync(password, user.password);
+
+		// 비밀번호 확인 실패
+		if (!result2) {
+			res.locals.status = 400;
+			res.locals.data = { message: '비밀번호가 일치하지 않습니다. 다시 한 번 확인해주세요.' };
+			next();
+			return false;
+		}
+
+		// 토큰 발급 (24h)
+		const token = jwt.sign({
+			type: 'JWT',
+			userKey: user.userKey,
+			id: user.id
+		}, jwtKey, {
+			expiresIn: '24h',
+			issuer: '발급자'
+		});
+		
+		// 쿠키 생성 (1w)
+		res.cookie('board_cookie', token, { domain: 'localhost', maxAge: 1000 * 60 * 60 * 24 * 7, httpOnly: true });
+
+		res.locals.status = 200;
+		res.locals.data = {
+			message: 'SUCCESS',
+			token: token
+		};
+		next();
 	}
 	catch (error) {
 		console.log('error', error);
-		res.status(500).send(error);
+		res.locals.status = 500;
+		res.locals.data = { message: '[01] 시스템 오류가 발생했습니다.' };
+		next();
 	}
 	finally {
 		await conn.release();
